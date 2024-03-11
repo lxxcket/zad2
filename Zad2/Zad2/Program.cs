c7a593e40f118cbd3154698fbab4e15841c12391
s27185 <s27185@pjwstk.edu.pl>
2024-03-11 09:25:50 +0100

    created main method

--- Zad2/Zad2/Program.cs
+++ Zad2/Zad2/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Modyfikacja 1");
-Console.WriteLine("Modyfikacja 2");
-Console.WriteLine("Modyfikacja 3");
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Average in array of ints is: ");
+    }
+}
