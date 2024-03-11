5c3b79f7604006225cf33be42bf7b06919c45c0b
s27185 <s27185@pjwstk.edu.pl>
2024-03-11 09:29:22 +0100

    implemented method avg

--- Zad2/Zad2/Program.cs
+++ Zad2/Zad2/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Average in array of ints is: ");
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.WriteLine("Average in array of ints is: " + avg(array));
+        
+        
+    }
+
+    public static int avg(int[] arr)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+            sum += arr[i];
+        return sum / arr.Length;
     }
 }
